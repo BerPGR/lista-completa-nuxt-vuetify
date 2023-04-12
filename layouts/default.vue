0d99dae4f7f969736e43fe9a8708a753d7bc26ea
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-04-12 16:05:22 -0300

    First design made for navigation drawer and app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app color="#9652FF">
+      <div class="text-center mt-10">
+        <v-avatar size="100">
+          <v-img :src="require('../assets/eu.png')"></v-img>
+        </v-avatar>
+        <p>Bernardo</p>
+      </div>
       <v-list>
+        <v-list-item-group>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
-          :to="item.to"
           router
           exact
         >
@@ -16,9 +22,19 @@
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app flat>
+    <v-app-bar color="transparent" app flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <h3>TODODEV</h3>
+      <v-spacer></v-spacer>
+      <h2 class="grey--text">SIGN OUT</h2>
+      <v-btn icon>
+        <v-icon>
+          mdi-logout
+        </v-icon>
+      </v-btn>  
     </v-app-bar>
     <v-main style="padding: 64px 0px 0px 0px">
       <v-content>
@@ -33,11 +49,14 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      miniVariant: false,
-      title: 'Vuetify.js',
+      items: [
+        {
+          title: 'Dashboard', icon: 'mdi-apps'
+        },
+        {
+          title: 'Settings', icon: 'mdi-cog'
+        }
+      ]
     }
   },
 }
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const { urlencoded } = require('express')
 const mongoose = require('mongoose')
 const { loadNuxt, build } = require('nuxt')
 const isDev = process.env.NODE_ENV !== 'production'
+require('dotenv').config()
+
+const db_user = process.env.DB_USER
+const db_pass = process.env.DB_PASSWORD
 
 const app = express()
 
@@ -12,7 +16,7 @@ app.use(urlencoded({
 
 app.use(express.json())
 
-mongoose.connect(`mongodb+srv://tiober007:FSz2oCZcwnH2Plg9@listona.qp0oi22.mongodb.net/pessoas?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${db_user}:${db_pass}@listona.qp0oi22.mongodb.net/pessoas?retryWrites=true&w=majority`)
 .then(async() => {
     const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
 
